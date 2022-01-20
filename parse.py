227655b2515c398e33f03215ac1498946a549392
Rémi Louf <remilouf@gmail.com>
2022-01-20 09:47:13 +0100

    Parse the items into the db

--- parse.py
+++ parse.py
@@ -8,11 +8,20 @@ def read_areas(conn, path):
     with open(path, "r") as datafile:
         reader = csv.reader(datafile, delimiter="\t")
         reader.__next__()  # skip column name
-
         with conn:
-            conn.executemany("INSERT INTO areas VALUES(?,?);", reader)
+            conn.executemany("INSERT OR IGNORE INTO areas VALUES(?,?);", reader)
+
+
+def read_items(conn, path):
+    with open(path, "r") as datafile:
+        reader = csv.reader(datafile, delimiter="\t")
+        reader.__next__()  # skip column name
+        items = [(row[2], row[3]) for row in reader]
+        with conn:
+            conn.executemany("INSERT OR IGNORE INTO items VALUES(?,?);", items)
 
 
 if __name__ == "__main__":
     conn = sqlite3.connect("us-prices.db")
     read_areas(conn, "data/raw/ap.area")
+    read_items(conn, "data/raw/ap.series")
